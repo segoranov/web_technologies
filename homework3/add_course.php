defe34be982fade71c6898c33f35c13bc54d2a49
Stiliyan Goranov <stiliqn.goranov@gmail.com>
2019-11-09 20:38:47 +0200

    validate credits

--- homework3/add_course.php
+++ homework3/add_course.php
@@ -5,29 +5,38 @@ $errors = array();
 if ($_POST) {
     $subject_name = $_POST['subject_name'];
     if (!$subject_name) {
-      $errors['subject_name'] = 'Името на избираемия предмет е задължително поле.';
+        $errors['subject_name'] = 'Името на избираемия предмет е задължително поле.';
     } elseif (strlen($subject_name) > 200) {
-      $errors['subject_name'] = 'Името на избираемия предмет има максимална дължина 150 символа.';
+        $errors['subject_name'] = 'Името на избираемия предмет има максимална дължина 150 символа.';
     } else {
-      $valid['subject_name'] = $subject_name;
+        $valid['subject_name'] = $subject_name;
     }
 
     $lecturer_name = $_POST['lecturer_name'];
     if (!$lecturer_name) {
-    $errors['lecturer_name'] = 'Името на лектора е задължително поле.';
+        $errors['lecturer_name'] = 'Името на лектора е задължително поле.';
     } elseif (strlen($lecturer_name) > 200) {
-    $errors['lecturer_name'] = 'Името на лектора има максимална дължина 200 символа.';
+        $errors['lecturer_name'] = 'Името на лектора има максимална дължина 200 символа.';
     } else {
-    $valid['lecturer_name'] = $lecturer_name;
+        $valid['lecturer_name'] = $lecturer_name;
     }
 
     $description = $_POST['description'];
     if (!$description) {
-    $errors['description'] = 'Описанието на избираемия предмет е задължително поле.';
+        $errors['description'] = 'Описанието на избираемия предмет е задължително поле.';
     } elseif (strlen($description) < 10) {
-    $errors['description'] = 'Описанието на избираемия предмет има минимална дължина 10 символа.';
+        $errors['description'] = 'Описанието на избираемия предмет има минимална дължина 10 символа.';
     } else {
-    $valid['description'] = $description;
+        $valid['description'] = $description;
+    }
+
+    $credits = $_POST['credits'];
+    if (!$credits) {
+        $errors['credits'] = 'Кредитите са задължително поле.';
+    } elseif (!is_numeric($credits) || $credits < 0) {
+        $errors['credits'] = 'Кредитите трябва да са цяло положително число.';
+    } else {
+        $valid['credits'] = $credits;
     }
 
     foreach ($errors as $error) {
