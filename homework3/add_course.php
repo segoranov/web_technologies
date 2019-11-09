cf59a957a19866ec47f6514aafb928df9922f518
Stiliyan Goranov <stiliqn.goranov@gmail.com>
2019-11-09 20:28:37 +0200

    validate subject and lecturer

--- homework3/add_course.php
+++ homework3/add_course.php
@@ -1,3 +1,29 @@
 <?php
-  echo 'Hello '.htmlentities($_POST["lecturer_name"]).'!';
+$valid = array();
+$errors = array();
+
+if ($_POST) {
+    $subject_name = $_POST['subject_name'];
+    if (!$subject_name) {
+      $errors['subject_name'] = 'Името на избираемия предмет е задължително поле.';
+    } elseif (strlen($subject_name) > 200) {
+      $errors['subject_name'] = 'Името на избираемия предмет има максимална дължина 150 символа.';
+    } else {
+      $valid['subject_name'] = $subject_name;
+    }
+
+    $lecturer_name = $_POST['lecturer_name'];
+    if (!$lecturer_name) {
+    $errors['lecturer_name'] = 'Името на лектора е задължително поле.';
+    } elseif (strlen($lecturer_name) > 200) {
+    $errors['lecturer_name'] = 'Името на лектора има максимална дължина 200 символа.';
+    } else {
+    $valid['lecturer_name'] = $lecturer_name;
+    }
+
+    foreach ($errors as $error) {
+        echo $error . "<br/>\n";
+    }
+
+}
 ?>
