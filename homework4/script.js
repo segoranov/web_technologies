606e08e6a51ce011f129d0bea976c2934df97aad
Stiliyan Goranov <stiliqn.goranov@gmail.com>
2020-01-04 21:21:10 +0200

    Almost done with hw4

--- homework4/script.js
+++ homework4/script.js
@@ -1,26 +1,59 @@
-const formSubmit = (event) => {
+
+const submitFormIfValid = (event) => {
     event.preventDefault();
 
-    const formElement = document.querySelector('#form');
+    if (validateForm()) {
+        let data = {
+            'username': document.forms["form"]["username"].value,
+            'password': document.forms["form"]["password"].value,
+            'repeat_password': document.forms["form"]["repeat_password"].value,
+        };
 
-    let data = {
-        'username': formElement.querySelector('input[name="username"]').value,
-        'password': formElement.querySelector('input[name="password"]').value,
-        'repeat_password': formElement.querySelector('input[name="repeat_password"]').value,
-    };
-
-    fetch('register.php', {
-        method: 'POST',
-        body: JSON.stringify(data),
-    })
-        .then(response => response.json())
-        .then(response => {
-            formElement.querySelector('input[name="username"]').value = response.username;
+        fetch('register.php', {
+            method: 'POST',
+            body: JSON.stringify(data),
         });
+    }
+}
+
+function validateForm() {
+    var username = document.forms["form"]["username"].value;
+    var password = document.forms["form"]["password"].value;
+    var repeat_password = document.forms["form"]["repeat_password"].value;
+
+    // TODO: 1 more validation for username
+    if (username.length < 3 || username.length > 10) {
+        document.getElementById('error_username').innerHTML = "Username length should be between 3 and 10 symbols";
+        return false;
+    } else {
+        document.getElementById('error_username').innerHTML = "OK";
+    }
+
+    if (password.length < 6) {
+        document.getElementById('error_password').innerHTML = "Password must contain at least 6 symbols";
+    } else {
+        document.getElementById('error_password').innerHTML = "OK";
+    }
+
+    if (!/\d/.test(password) || !/[a-z]/.test(password) || !/[A-Z]/.test(password)) {
+        document.getElementById('error_password').innerHTML
+            = "Password must contain at least one digit, upper case letter and lower case letter";
+        return false;
+    } else {
+        document.getElementById('error_password').innerHTML = "OK";
+    }
+
+    if (repeat_password != password) {
+        document.getElementById('error_repeat_password').innerHTML = "Passwords do not match";
+        return false;
+    } else {
+        document.getElementById('error_repeat_password').innerHTML = "OK";
+    }
+
+
+    return true;
 }
 
 window.addEventListener('load', () => {
-    document.getElementById('form').addEventListener('submit', formSubmit);
-
-    // document.querySelector('#content').addEventListener('click', onClickParent);
+    document.getElementById('form').addEventListener('submit', submitFormIfValid);
 });
